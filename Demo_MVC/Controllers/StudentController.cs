6485f1a5dd3457a5fdbce234a6cba04f4c91b2b5
“DANGVPHAM” <dangpv21022003@gmail.com>
2025-02-24 13:10:12 +0700

    BTAP

---
+++ Demo_MVC/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Demo_MVC.Models;
+namespace Demo_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student std = new Student();
+            std.Id = "1";
+            std.FullName = "Nguyen Van A";
+            return View(std);       
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            ViewBag.Message = "ID:" + std.Id + " FullName:" + std.FullName;
+            return View();
+        }
+    }
+}
